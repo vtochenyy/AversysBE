082f21caaaa19d1a44354957d124b060ce00ea37
Vitaly Tochenyy <Antisvinn@yadnex.by>
2022-11-05 14:45:30 +0300

    findById endpoint was created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,9 +5,11 @@ import { UserService } from './users.service';
 
 export class UsersController extends BaseController {
 	DBSchema: any;
+	userService: UserService;
 	constructor(logger: LoggerService, DBSchema: {}) {
 		super(logger);
 		this.DBSchema = DBSchema;
+		this.userService = new UserService();
 		this.bindUserAPI();
 	}
 
@@ -27,8 +29,32 @@ export class UsersController extends BaseController {
 				func: async (req, res, next) => {
 					if (!!req.body.firstName && !!req.body.lastName && !!req.body.age) {
 						try {
-							let result = await new UserService().createRecord(req.body, this.DBSchema.User);
+							let result = await this.userService.createRecord.bind(this.userService)(
+								req.body,
+								this.DBSchema.User,
+								next
+							);
 							res.status(200).send(result);
+						} catch (error) {
+							next(new HttpError(500, 'Ошибка обработки запроса', '/testUser'));
+						}
+					} else {
+						next(new HttpError(500, 'Ошибка структуры тела запроса', '/testUser'));
+					}
+				},
+			},
+			{
+				path: '/findById',
+				method: 'post',
+				func: async (req, res, next) => {
+					if (!!req.body.id) {
+						try {
+							let result = await this.userService.getUserById.bind(this.userService)(
+								req.body.id,
+								this.DBSchema.User,
+								next
+							);
+							!!result && res.status(200).send(result);
 						} catch (error) {
 							next(new HttpError(500, 'Ошибка обработки запроса', '/testUser'));
 						}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,31 @@
-import { Sequelize } from 'sequelize';
+import { NextFunction } from 'express';
 import { HttpError } from '../errors/http-error.class';
 import { IUserDto } from './user.dto.interface';
 
 export class UserService {
-	async createRecord(params: IUserDto, userEntity: any) {
+	async createRecord(params: IUserDto, userEntity: any, next: NextFunction) {
 		try {
 			const user = await userEntity.create(params);
 			return { id: user.id };
 		} catch (error) {
-			new HttpError(500, 'Service error', 'UserService');
+			next(new HttpError(500, 'Service error', 'UserService'));
+		}
+	}
+
+	async getUserById(userID: string, userEntity: any, next: NextFunction) {
+		try {
+			const user = await userEntity.findAll({
+				where: {
+					id: userID,
+				},
+			});
+			if (user.length == 0) {
+				throw new Error();
+			} else {
+				return user;
+			}
+		} catch (error) {
+			next(new HttpError(500, 'User not found by id', 'UserService'));
 		}
 	}
 }
