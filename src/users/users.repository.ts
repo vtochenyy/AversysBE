304ff08596ce6c226421651584692024f96622ee
Vitaly Tochenyy <Antisvinn@yandex.by>
2023-01-02 19:57:01 +0300

    .prettierrc was added

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,47 +1,55 @@
-import { PrismaClient, UserModel } from '@prisma/client';
-import { v1 as uuidv1 } from 'uuid';
-import { inject, injectable } from 'inversify';
-import { IUsersRepository } from './usersRepositoty.interface';
-import { TYPES } from '../types';
-import { IDatabaseService } from '../db/databaseService.interface';
+import { PrismaClient, UserModel } from '@prisma/client'
+import { v1 as uuidv1 } from 'uuid'
+import { inject, injectable } from 'inversify'
+import { IUsersRepository } from './usersRepositoty.interface'
+import { TYPES } from '../types'
+import { IDatabaseService } from '../db/databaseService.interface'
 
 @injectable()
 export class UsersRepository implements IUsersRepository {
-	user: UserModel;
-	client: PrismaClient;
+    user: UserModel
+    client: PrismaClient
 
-	constructor(@inject(TYPES.DatabaseService) private databaseService: IDatabaseService) {
-		this.client = this.databaseService.client;
-	}
+    constructor(@inject(TYPES.DatabaseService) private databaseService: IDatabaseService) {
+        this.client = this.databaseService.client
+    }
 
-	async create(params: UserModel) {
-		params.id = uuidv1();
-		let user = await this.client.userModel.create({ data: params });
-		return user;
-	}
+    async create(params: UserModel) {
+        params.id = uuidv1()
+        let user = await this.client.userModel.create({ data: params })
+        return user
+    }
 
-	async update(userId: string, userData: UserModel) {
-		return await this.client.userModel.update({ where: { id: userId }, data: userData });
-	}
+    async update(userId: string, userData: UserModel) {
+        let result = await this.client.userModel.update({
+            where: { id: userId },
+            data: userData,
+        })
+        return result
+    }
 
-	async findById(userId: string) {
-		return await this.client.userModel.findUnique({ where: { id: userId } });
-	}
+    async findById(userId: string) {
+        let result = await this.client.userModel.findUnique({
+            where: { id: userId },
+        })
+        return result
+    }
 
-	async findByCriteria(userData: any): Promise<UserModel[]> {
-		return await this.client.userModel.findMany({
-			where: {
-				...userData,
-			},
-		});
-	}
+    async findByCriteria(userData: any): Promise<UserModel[]> {
+        let result = await this.client.userModel.findMany({
+            where: {
+                ...userData,
+            },
+        })
+        return result
+    }
 
-	async deleteById(userId: string) {
-		try {
-			this.client.userModel.delete({ where: { id: userId } });
-			return true;
-		} catch (err) {
-			return false;
-		}
-	}
+    async deleteById(userId: string) {
+        try {
+            this.client.userModel.delete({ where: { id: userId } })
+            return true
+        } catch (err) {
+            return false
+        }
+    }
 }
--- src/users/usersLogs/usersLogs.repository.ts
+++ src/users/usersLogs/usersLogs.repository.ts
@@ -1,23 +1,26 @@
-import { PrismaClient, UsersLogsModel } from '@prisma/client';
-import { inject, injectable } from 'inversify';
-import { IDatabaseService } from '../../db/databaseService.interface';
-import { TYPES } from '../../types';
-import { IUsersLogsRepository } from './usersLogsRepository.interface';
-import 'reflect-metadata';
+import { PrismaClient, UsersLogsModel } from "@prisma/client";
+import { inject, injectable } from "inversify";
+import { IDatabaseService } from "../../db/databaseService.interface";
+import { TYPES } from "../../types";
+import { IUsersLogsRepository } from "./usersLogsRepository.interface";
+import "reflect-metadata";
 
 @injectable()
 export class UsersLogsRepository implements IUsersLogsRepository {
-	client: PrismaClient;
+  client: PrismaClient;
 
-	constructor(@inject(TYPES.DatabaseService) private databaseService: IDatabaseService) {
-		this.client = this.databaseService.client;
-	}
+  constructor(
+    @inject(TYPES.DatabaseService) private databaseService: IDatabaseService
+  ) {
+    this.client = this.databaseService.client;
+  }
 
-	async create(params: { id: string; message: string, userId: string }) {
-		let data = await this.client.usersLogsModel.create({ data: params });
-	}
+  async create(params: { id: string; message: string; userId: string }) {
+    let data = await this.client.usersLogsModel.create({ data: params });
+  }
 
-	async getAll(): Promise<UsersLogsModel[]> {
-		return await this.client.usersLogsModel.findMany();
-	}
+  async getAll(): Promise<UsersLogsModel[]> {
+    let result = await this.client.usersLogsModel.findMany();
+    return result;
+  }
 }
